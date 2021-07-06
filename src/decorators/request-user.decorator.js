6845e866384a788be124b11a18a2ae91b6e197bd
Vladyslav Koropets <vladyslav.koropets@onix-systems.com>
2021-07-06 18:41:59 +0300

    feat(clear): rm js files

--- src/decorators/request-user.decorator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var common_1 = require("@nestjs/common");
-var RequestUser = common_1.createParamDecorator(function (data, ctx) {
-    var request = ctx.switchToHttp().getRequest();
-    return request.user;
-});
-exports["default"] = RequestUser;
